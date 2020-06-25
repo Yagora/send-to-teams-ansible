3a263621f51e72abb1cf08207442e1aa11a94409
Yagora <tanguy.charon47@gmail.com>
2020-06-25 23:30:19 +0200

    Init send_to_teams ansible module

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# send-to-teams-ansible
+# Send a message to a webhook teams
+This action_plugin can be setup on your project if you want to send a message to a teams webhook.
+
+## How to use it ?
+
+Download the action_modules folder to your Ansible project root. After that you can use the plugin like this :
+
+```yaml
+    - name: send build result notification to teams when succed
+      send_to_teams:
+        web_hook: "https://outlook.office.com/webhook/ID"
+        success:  True
+        title: "Build"
+        environment: "{{ your_env }}"
+        action: "I tried to build something"
+```
+## Result
+
+![Build Success](/send_to_teams_success.png)
---
+++ action_plugins/send_to_teams.py
@@ -0,0 +1,52 @@
+import requests
+import json
+import sys
+import boto3
+from ansible.plugins.action import ActionBase
+
+
+############################## SEND TO TEAMS ##############################
+## This module can send message to a teams webhook
+###########################################################################
+
+SUCCESS_COLOR = "00CC00"
+FAILED_COLOR = "FF0000"
+
+class ActionModule(ActionBase):
+    def run(self, tmp=None, task_vars=None):
+
+        web_hook = self._task.args.get("web_hook")
+        title = self._task.args.get("title")
+        success = self._task.args.get("success")
+        environment = self._task.args.get("environment")
+        action = self._task.args.get("action")
+
+        if success:
+            message_color = SUCCESS_COLOR
+            job_status = "Success"
+        else:
+            job_status = "Failure"
+            message_color = FAILED_COLOR
+
+        fullTitle = "[" + title + "] - " + action + " in " + environment + " - " + job_status
+
+        teams_params = {
+            "summary": fullTitle,
+            "themeColor": message_color,
+            "sections": [{
+                    "title":  fullTitle,
+                    "facts": [{
+                        "name": "Environments",
+                        "value": environment
+                    }, {
+                        "name": "Action",
+                        "value": action
+                    }, {
+                        "name": "Status",
+                        "value": job_status
+                    }]
+            }]
+        }
+
+        requests.post(web_hook, data=json.dumps(teams_params))
+        return {}
